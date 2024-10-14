85b5fc4579b2e81998bb24069ab29ec0e7d76adb
Shahriar-Khan-Dhrubo <dhrubo133362@gmail.com>
2024-10-15 01:05:34 +0600

    nested mapping completed

--- nested-mapping/src/App.js
+++ nested-mapping/src/App.js
@@ -4,45 +4,38 @@ const users = [
   {
     name: "Msk Dhrubo",
     age: 27,
-    number: [
-      {home : "50646477"},
-      {office : "520664460"}
-    ]
+    number: [{ home: "50646477" }, { office: "520664460" }],
   },
   {
-    name: "Msk Dhrubo",
-    age: 27,
-    number: [
-      {home : "50646477"},
-      {office : "520664460"}
-    ]
+    name: "Arifa Anjum Ava",
+    age: 25,
+    number: [{ home: "369467947477" }, { office: "5206644363660" }],
   },
   {
-    name: "Msk Dhrubo",
-    age: 27,
-    number: [
-      {home : "50646477"},
-      {office : "520664460"}
-    ]
+    name: "Msk Dhoni",
+    age: 8,
+    number: [{ home: "557997477" }, { office: "6407057" }],
   },
 ];
 
 function App() {
   return (
     <div className="App">
-      {users.map((user,index) => {
-        return <article key={index}>
-          <h3>{user.name}</h3>
-          <p>{user.age}</p>
-          {
-            user.number.map((phone,index)=>{
-              return <div key={index}>
-                <p>Home : {phone.home}</p>
-                <p>Office : {phone.office}</p>
-              </div>
-            })
-          }
-        </article>
+      {users.map((user, index) => {
+        return (
+          <article key={index}>
+            <p>Fullname : {user.name}</p>
+            <p>Age : {user.age}</p>
+            {user.number.map((phone, index) => {
+              return (
+                <div key={index}>
+                  {phone.home && <p>Home: {phone.home}</p>}
+                  {phone.office && <p>Office: {phone.office}</p>}
+                </div>
+              );
+            })}
+          </article>
+        );
       })}
     </div>
   );

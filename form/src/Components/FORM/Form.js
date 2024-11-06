ddd2f846ce496609f54ab61d56c59db5992a2d36
Shahriar-Khan-Dhrubo <dhrubo133362@gmail.com>
2024-11-07 02:59:35 +0600

    state shifting

--- form/src/Components/FORM/Form.js
+++ form/src/Components/FORM/Form.js
@@ -3,26 +3,39 @@ import style from '../FORM/form.module.css'
 
 export default function Form() {
 
-  const [name,setName] = useState("");
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+  // const [name,setName] = useState("");
+  // const [email,setEmail] = useState("");
+  // const [password,setPassword] = useState("");
 
-  const handleName = (e) =>{
-    setName(e.target.value || "");
+  const [user,setUser] = useState({name:'',email:'',password:''});
+  const {name,email,password} = user;
+
+  // const handleName = (e) =>{
+  //   setUser({name:e.target.value,email,password});
+  // }
+  // const handleEmail = (e) =>{
+  //   setUser({name,email:e.target.value,password});
+  // }
+  // const handlePassword = (e) =>{
+  //   setUser({name,email,password:e.target.value});
+  // }
+
+  const handleChange = (e)=>{
+    // const filledName = e.target.name;
+    // if(filledName==='name'){
+    //   setUser({name:e.target.value,email,password});
+    // }
+    // else if(filledName === 'email'){
+    //   setUser({name,email:e.target.value,password});
+    // }
+    // else{
+    //   setUser({name,email,password:e.target.value});
+    // }
+    setUser({...user,[e.target.name]:e.target.value})
   }
-  const handleEmail = (e) =>{
-    setEmail(e.target.value || "");
-  }
-  const handlePassword = (e) =>{
-    setPassword(e.target.value || "");
-  }
+
   const handleSubmit = (e) =>{
-    let userInfo={
-      name,
-      email,
-      password,
-    }
-    console.log(userInfo);
+    console.log(user);
     e.preventDefault();
   }
 
@@ -32,15 +45,15 @@ export default function Form() {
       <form onSubmit={handleSubmit}>
         <div className={style.formgroup}>
             <label htmlFor='name'>Name : </label>
-            <input type='text' name='name' id='name' value={name} autoComplete='name' onChange={handleName}></input>
+            <input type='text' name='name' id='name' value={name} autoComplete='name' onChange={handleChange}></input>
         </div>
         <div className={style.formgroup}>
             <label htmlFor='email'>Email : </label>
-            <input type='text' name='email' id='email' value={email} autoComplete='email' onChange={handleEmail}></input>
+            <input type='text' name='email' id='email' value={email} autoComplete='email' onChange={handleChange}></input>
         </div>
         <div className={style.formgroup}>
             <label htmlFor='password'>Password : </label>
-            <input type='password' name='password' id='password' value={password} autoComplete='new-password' onChange={handlePassword}></input>
+            <input type='password' name='password' id='password' value={password} autoComplete='new-password' onChange={handleChange}></input>
         </div>
         <div className={style.formgroup}>
             <button type='submit'>REGISTER</button>

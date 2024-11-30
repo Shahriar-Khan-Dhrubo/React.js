e2b6f63930914e0e1b9ce589aef3c0ae71a03ade
Shahriar-Khan-Dhrubo <dhrubo133362@gmail.com>
2024-11-30 12:39:20 +0600

    added some missed codes

--- Projects/project-1-todo-app/src/Components/NewTodo.js
+++ Projects/project-1-todo-app/src/Components/NewTodo.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./NewTodo.module.css";
 
 export default function NewTodo() {
+
+  const [todo,setTodo]=useState({title:'',desc:''});
+  const {title,desc} = todo;
+
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+  }
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={handleSubmit}>
       <div className={style["form-field"]}>
         <label htmlFor="title">Title : </label>
-        <input type="text" id="title" name="name"></input>
+        <input type="text" id="title" name="name" value={title} onChange={handleChange}></input>
       </div>
       <div className={style["form-field"]}>
         <label htmlFor="title">Description : </label>
-        <textarea type="text" id="desc" name="desc"></textarea>
+        <textarea type="text" id="desc" name="desc" value={desc} onChange={handleChange}></textarea>
       </div>
       <button type="submit">Add Todo</button>
     </form>
